c14263982586f15a56938d1a6eeeda2fe3c02e42
Louis <loulamia@mit.edu>
2018-02-12 11:40:20 -0500

    separate block creation/voting pause/resume
    [PR 92 Part 6/8] Source: https://github.com/jpmorganchase/quorum/pull/92/commits/fb0d80ce55775cc6169adf20fe91d68f5ae21ed4

--- core/quorum/api.go
+++ core/quorum/api.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	Strategy BlockMakerStrategy
+	Strategy BlockVoteMakerStrategy
 )
 
 type PublicQuorumAPI struct {
@@ -106,14 +106,28 @@ func (api *PublicQuorumAPI) IsBlockMaker(addr common.Address) (bool, error) {
 
 func (api *PublicQuorumAPI) PauseBlockMaker() error {
 	if Strategy != nil {
-		return Strategy.Pause()
+		return Strategy.PauseBlockMaking()
 	}
 	return nil
 }
 
 func (api PublicQuorumAPI) ResumeBlockMaker() error {
 	if Strategy != nil {
-		return Strategy.Resume()
+		return Strategy.ResumeBlockMaking()
+	}
+	return nil
+}
+
+func (api PublicQuorumAPI) PauseVoting() error {
+	if Strategy != nil {
+		return Strategy.PauseVoting()
+	}
+	return nil
+}
+
+func (api PublicQuorumAPI) ResumeVoting() error {
+	if Strategy != nil {
+		return Strategy.ResumeVoting()
 	}
 	return nil
 }
